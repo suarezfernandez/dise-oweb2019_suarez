95f58b6bf03e9a52cc14537d8fed3517234b7149
luisfernandez <luisfernandez0793@gmail.com>
2019-06-05 12:11:54 -0400

    junio

---
+++ javascript_101/0_script.js
@@ -0,0 +1,38 @@
+var casa= 23;
+var dueno= "pedro";
+alert("hola a todos!");
+/*pedir al usuario que ingrese su edad*/
+
+
+//soy un comentario de una 1 sola linea
+//pedir al usuario que digite su nombre
+
+var nombre = prompt("Digite su nombre");
+
+var edad = prompt("Digite su edad");
+
+var ciudad = prompt("Digite su ciudad");
+
+var tarjeta_credito = prompt("Digite su tarjeta de credito"); 
+
+//imprime los valores por la consola
+console.log(nombre);
+console.log(edad);
+console.log(ciudad);
+console.log(tarjeta_credito);
+
+console.log(document.documentURI);
+console.log(document.body);
+console.log(document.head);
+console.log(document.height);
+console.log(document.title);
+
+//esta variable no esta definida
+var pais;
+console.log(pais)
+
+var pais = "rep. dom.";
+console.log(pais);
+
+//cambiar el titulo
+document.title = "Nuevo titulo";
